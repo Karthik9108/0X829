cf6f0ff7ff81c99121a4cf77df6a5f3058a580a8
Karthik9108 <91299937+Karthik9108@users.noreply.github.com>
2022-11-25 22:17:37 +0530

    Initialize feature/validation

---
+++ assembly/index.ts
@@ -0,0 +1,169 @@
+import { JSON } from "assemblyscript-json";
+import {Position, parsePrices, getTickFromPrice, trailingStop, renderULMResult} from "@steerprotocol/strategy-utils";
+
+
+let width: i32 = 600;
+let percent: f32 = 0;
+let poolFee: i32 = 0;
+
+export function initialize(config: string): void {
+  // Parse the config object
+  const configJson = <JSON.Obj>JSON.parse(config);
+  // Get our config variables
+  const _width = configJson.getInteger("binWidth");
+  const _poolFee = configJson.getInteger("poolFee");
+  const _percent = configJson.getValue("percent");
+  // Handle null case
+  if (_width == null || _percent == null || _poolFee == null) {
+    throw new Error("Invalid configuration");
+  }
+
+  // Handle int percents
+  if (_percent.isFloat) {
+    const f_percent = <JSON.Num>_percent
+    percent = f32(f_percent._num);
+  }
+  if (_percent.isInteger) {
+    const i_percent = <JSON.Integer>_percent
+    percent = f32(i_percent._num);
+  }
+  // Assign values to memory
+  width = i32(_width._num);
+  poolFee = i32(_poolFee._num);
+}
+
+export function execute(_prices: string): string {
+  // _prices will be a nested array with the OHLC data in the deeper array,
+  const prices = parsePrices(_prices);
+  // If we have no candles 
+  if (prices.length == 0) {return `skip tend, no candles`}
+  // Get Trailing stop price
+  const trailingLimit = trailingStop(percent, prices)
+  // Calculate position
+  const positions = calculateBin(trailingLimit);
+  // Format and return result
+  return renderULMResult(positions);
+}
+
+
+function calculateBin(upper: f32): Position[] {
+
+  // Calculate the upper tick based on the start of the stop
+  const upperTick: i32 = i32(Math.round(getTickFromPrice(upper)));
+
+  // Step down ticks until we reach an initializable tick
+  let _startTick: i32 = upperTick;
+  while (_startTick % poolFee  !== 0) {
+    _startTick--;
+  }
+
+  const positions: Array<Position> = [];
+  const position = new Position(_startTick - width, _startTick, 1);
+  positions.push(position);
+
+  return positions
+}
+
+export function config(): string{
+  return `{
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "title": "Strategy Config",
+    "type": "object",
+    "properties": {
+      "percent": {
+          "type": "number",
+          "description": "Percent for trailing stop order"
+      },
+      "poolFee": {
+        "type": "number",
+        "description": "Pool fee percent for Uniswapv3 pool",
+        "enum": [1,10,60,200],
+        "enumNames" : ["Lowest 0.01%", "Low 0.05%", "Medium 0.3%", "High 1%"]
+      }
+    },
+    "required": ["percent",  "poolFee"],
+    "allOf": [
+      {
+        "if" : {
+          "properties": {
+            "poolFee" : {
+              "const" : 200
+            }
+          }
+      },
+      "then" : {
+        "properties": {
+          "binWidth": {
+            "type": "integer",
+          "description": "Width for liquidity position, must be a multiple of pool tick spacing",
+          "multipleOf": 200,
+          "minimum": 200
+          }
+        },
+        "required": ["binWidth"]
+      }
+      },
+      {
+        "if" : {
+          "properties": {
+            "poolFee" : {
+              "const" : 60
+            }
+          }
+      },
+      "then" : {
+        "properties": {
+          "binWidth": {
+            "type": "integer",
+          "description": "Width for liquidity position, must be a multiple of pool tick spacing",
+          "multipleOf": 60,
+          "minimum": 60
+          }
+        },
+        "required": ["binWidth"]
+      }
+      },
+      {
+        "if" : {
+          "properties": {
+            "poolFee" : {
+              "const" : 10
+            }
+          }
+      },
+      "then" : {
+        "properties": {
+          "binWidth": {
+            "type": "integer",
+          "description": "Width for liquidity position, must be a multiple of pool tick spacing",
+          "multipleOf": 10,
+          "minimum": 10
+          }
+        },
+        "required": ["binWidth"]
+      }
+      },
+      {
+        "if" : {
+          "properties": {
+            "poolFee" : {
+              "const" : 1
+            }
+          }
+      },
+      "then" : {
+        "properties": {
+          "binWidth": {
+            "type": "integer",
+          "description": "Width for liquidity position, must be a multiple of pool tick spacing",
+          "multipleOf": 1,
+          "minimum": 1
+          }
+        },
+        "required": ["binWidth"]
+      }
+      }
+    ]
+  }`;
+}
+
